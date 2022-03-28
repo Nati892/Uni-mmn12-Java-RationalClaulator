5e91727c1a43a514eb7d51c66b870a76019abc58
Nati892 <netanel892@gmail.com>
2022-03-28 11:35:45 +0300

    created repo

---
+++ src/com/company/Rational.java
@@ -0,0 +1,118 @@
+package com.company;
+
+public class Rational {
+
+    private int _numerator, _denominator;
+
+    public Rational(int numerator, int denominator) throws IllegalArgumentException {
+        this._numerator = numerator;
+        this._denominator = 1;
+        if (denominator <= 0)
+            throw new IllegalArgumentException("Denominator must be Natural number");
+
+        this._denominator = denominator;
+
+    }
+
+    public int getNumerator() {
+        return this._numerator;
+    }
+
+    public int getDenominator() {
+        return this._denominator;
+    }
+
+
+    public boolean greaterThan(Rational other) {
+        return ((this._numerator * other.getDenominator()) > (this._denominator * other.getNumerator()));
+    }
+
+
+    public boolean equals(Rational other) {
+        return ((this._numerator * other.getDenominator()) == (this._denominator * other.getNumerator()));
+    }
+
+    public Rational plus(Rational other) {
+        Rational sum_rationals = null;
+        int new_numerator = (this._numerator * other.getDenominator()) + (this._denominator * other.getNumerator());
+        int new_denominator = this._denominator * other.getDenominator();
+        try {
+            sum_rationals = new Rational(new_numerator, new_denominator);
+        } catch (IllegalArgumentException e) {
+            System.out.println("At least one of your numbers has a denominator that equals zero.");
+            e.printStackTrace();
+        }
+        return sum_rationals;
+
+    }
+
+    public Rational minus(Rational other) {
+        Rational sum_rationals = null;
+        int new_numerator = (this._numerator * other.getDenominator()) - (this._denominator * other.getNumerator());
+        int new_denominator = this._denominator * other.getDenominator();
+        try {
+            sum_rationals = new Rational(new_numerator, new_denominator);
+        } catch (IllegalArgumentException e) {
+            System.out.println("At least one of your numbers has a denominator that equals zero.");
+            e.printStackTrace();
+        }
+        return sum_rationals;
+    }
+
+    public Rational multiply(Rational other) {
+        Rational sum_rationals = null;
+        int new_numerator = (this._numerator * other.getNumerator());
+        int new_denominator = this._denominator * other.getDenominator();
+        try {
+            sum_rationals = new Rational(new_numerator, new_denominator);
+        } catch (IllegalArgumentException e) {
+            System.out.println("At least one of your numbers has a denominator that equals zero.");
+            e.printStackTrace();
+        }
+        return sum_rationals;
+    }
+
+    public Rational divide(Rational other) throws ArithmeticException {//did not use multiply here because of unfitting error message
+        Rational sum_rationals = null;
+        if (other.getNumerator() == 0)
+            throw new ArithmeticException("Denominator parameter is zero");
+        int new_numerator = (this._numerator * other.getDenominator());
+        int new_denominator = this._denominator * other.getNumerator();
+        try {
+            sum_rationals = new Rational(new_numerator, new_denominator);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Returned number will have a denominator of zero");
+            e.printStackTrace();
+        }
+        return sum_rationals;
+    }
+
+
+    public Rational reduce() {
+        Rational reduce_rational = null;
+        int this_gcd = gcd(this._numerator, this._denominator);
+        int new_numerator = (this._numerator / this_gcd);
+        int new_denominator = (this._denominator / this_gcd);
+
+        try {
+            reduce_rational = new Rational(new_numerator, new_denominator);
+        } catch (IllegalArgumentException e) {
+            System.out.println("reduction results in a denominator of zero");
+            e.printStackTrace();
+        }
+        return reduce_rational;
+    }
+
+    private int gcd(int x, int y) {
+        if (y == 0)
+            return x;
+        return gcd(y, x % y);
+    }
+
+    @Override
+    public String toString() {
+        String result = this._numerator + "/" + this._denominator;
+        return result;
+    }
+
+}
---
+++ src/com/company/RationalCalculator.java
@@ -0,0 +1,62 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class RationalCalculator {
+
+    private static Scanner scanner;
+
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+        inputLoop();
+        System.out.println("Goodbye, thanks for playing.");
+
+    }
+
+
+    public static void inputLoop() {
+
+        while (true) {/*main loop for program, will exit when user enters exit*/
+            System.out.println("Lets start!,Enter \"exit\" at any time to exit");
+            System.out.println("please enter first rational number ");
+            String user_input = GetUserInput();
+            Rational first, second;
+            char operation;
+
+            first = handleRationalInput(user_input);//get first rational number
+            while (first == null) {//if the input was bad then null is returned and the user should renter the first
+                System.out.println("please reenter the first rational number");
+                user_input = GetUserInput();
+                first = handleRationalInput(user_input);
+
+            }
+
+
+
+
+
+        }
+
+
+    }
+
+    public static Rational handleRationalInput(String text) {
+        System.out.println("Debug: " + text);/*debug*/
+
+    }
+
+    public static String GetUserInput() {
+        String user_input = scanner.nextLine().trim();
+
+        if (user_input.equals("exit"))
+            System.exit(0);
+
+        if (user_input.equals(""))
+            return GetUserInput();
+
+        return user_input;
+
+    }
+
+
+}
