7257abd2374d73bda37614aaa5c79c8b8a37d55a
Nati892 <netanel892@gmail.com>
2022-03-28 18:22:24 +0300

    done + documentation

--- src/com/company/Rational.java
+++ src/com/company/Rational.java
@@ -4,6 +4,13 @@ public class Rational {
 
     private int _numerator, _denominator;
 
+    /**
+     * constructor for Rational number
+     *
+     * @param numerator   - the numerator of the rational number
+     * @param denominator - the denominator of the rational number
+     * @throws IllegalArgumentException for a denominator that equals zero
+     */
     public Rational(int numerator, int denominator) throws IllegalArgumentException {
         this._numerator = numerator;
         this._denominator = 1;
@@ -14,24 +21,50 @@ public class Rational {
 
     }
 
+    /**
+     * getter for Numerator
+     *
+     * @return the Rational's numerator
+     */
     public int getNumerator() {
         return this._numerator;
     }
 
+    /**
+     * getter for denominator
+     *
+     * @return the Rational's denominator
+     */
     public int getDenominator() {
         return this._denominator;
     }
 
-
+    /**
+     * this function returns whether or not this Rational number is bigger then the given param
+     *
+     * @param other param to compare to
+     * @return boolean indicating whether or not this Rational number is bigger then the given param
+     */
     public boolean greaterThan(Rational other) {
         return ((this._numerator * other.getDenominator()) > (this._denominator * other.getNumerator()));
     }
 
-
+    /**
+     * this function returns whether or not this Rational number and the param are equal
+     *
+     * @param other param to compare to
+     * @return boolean indicating whether or not this Rational number and the param are equal
+     */
     public boolean equals(Rational other) {
         return ((this._numerator * other.getDenominator()) == (this._denominator * other.getNumerator()));
     }
 
+    /**
+     * this function addition between this Rational and the param
+     *
+     * @param other param to add to
+     * @return a new result Rational number
+     */
     public Rational plus(Rational other) {
         Rational sum_rationals = null;
         int new_numerator = (this._numerator * other.getDenominator()) + (this._denominator * other.getNumerator());
@@ -45,6 +78,12 @@ public class Rational {
 
     }
 
+    /**
+     * this function subtraction between this Rational and the param
+     *
+     * @param other param to subtract with
+     * @return a new result Rational number
+     */
     public Rational minus(Rational other) {
         Rational sum_rationals = null;
         int new_numerator = (this._numerator * other.getDenominator()) - (this._denominator * other.getNumerator());
@@ -57,6 +96,12 @@ public class Rational {
         return sum_rationals;
     }
 
+    /**
+     * this function multiplication between this Rational and the param
+     *
+     * @param other param to multiply with
+     * @return a new result Rational number
+     */
     public Rational multiply(Rational other) {
         Rational sum_rationals = null;
         int new_numerator = (this._numerator * other.getNumerator());
@@ -69,6 +114,13 @@ public class Rational {
         return sum_rationals;
     }
 
+    /**
+     * this function divides between this Rational and the param
+     *
+     * @param other param to divide with
+     * @return a new result Rational number
+     * @throws ArithmeticException for a param that equals 0
+     */
     public Rational divide(Rational other) throws ArithmeticException {//did not use multiply here because of unfitting error message
         Rational sum_rationals = null;
         if (other.getNumerator() == 0)
@@ -83,7 +135,11 @@ public class Rational {
         return sum_rationals;
     }
 
-
+    /**
+     * this function reduces this Rational
+     *
+     * @return a new result Rational
+     */
     public Rational reduce() {
         Rational reduce_rational = null;
         int this_gcd = gcd(this._numerator, this._denominator);
@@ -99,6 +155,10 @@ public class Rational {
     }
 
     private int gcd(int x, int y) {
+        if (x < 0)
+            x *= -1;
+        if (y < 0)
+            y *= -1;
         if (y == 0)
             return x;
         return gcd(y, x % y);
@@ -106,7 +166,9 @@ public class Rational {
 
     @Override
     public String toString() {
+
         String result = this._numerator + "/" + this._denominator;
+
         return result;
     }
 
--- src/com/company/RationalCalculator.java
+++ src/com/company/RationalCalculator.java
@@ -8,48 +8,61 @@ public class RationalCalculator {
 
     private static Scanner scanner;
 
+    /**
+     * entry point for program.
+     * the functions calls for inputLoop to start the calculator
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-        scanner = new Scanner(System.in);
-        inputLoop();
+        scanner = new Scanner(System.in);//init scanner
+        inputLoop();//main loop
         System.out.println("Goodbye, thanks for using my calculator.");
 
     }
 
-
+    /**
+     * This is the main loop of the program.
+     * gets two Rational numbers and shows in the end all the operations on them.
+     */
     public static void inputLoop() {
 
         while (true) {/*main loop for program, will exit when user enters exit*/
             System.out.println("Lets start!,Enter \"exit\" at any time to exit");
             System.out.println("please enter first rational number ");
-            Rational first, second;
+            Rational first_num, second_num;
 
 
-            first = getRationalNumber();//get first rational number
-            while (first == null) {//if the input was bad then null is returned and the user should renter the first
+            first_num = getRationalNumber();//get first rational number
+            while (first_num == null) {//if the input was bad then null is returned and the user should renter the first
                 System.out.println("please re-enter the first rational number");
-                first = getRationalNumber();
+                first_num = getRationalNumber();
             }
 
-            second = getRationalNumber();//get first rational number
-            while (second == null) {//if the input was bad then null is returned and the user should renter the first
+            second_num = getRationalNumber();//get first rational number
+            while (second_num == null) {//if the input was bad then null is returned and the user should renter the second
                 System.out.println("please re-enter the second rational number");
-                second = getRationalNumber();
+                second_num = getRationalNumber();
             }
 
-            showOperations(first, second);
+            showOperations(first_num, second_num);//shows the result of each operation defined in the Rational class
         }
 
 
     }
 
+    /**
+     * This function gets a Rational number from the user
+     *
+     * @return the Rational number that was entered from the user
+     */
     public static Rational getRationalNumber() {
-        boolean good_rational_number = false;
-
-        Rational returned_rational = null;
+        boolean good_rational_number = false;//used for the try-catch loop
+        Rational returned_rational = null;//the returned Rational number
 
         int numerator;
         int denominator;
-        while (!good_rational_number) {
+        while (!good_rational_number) {//try-catch loop trying to get two integers from the user
             System.out.println("please enter a numerator");
             numerator = getUserInt();
             System.out.println("please enter a denominator");
@@ -57,7 +70,7 @@ public class RationalCalculator {
 
             try {
                 returned_rational = new Rational(numerator, denominator);
-                good_rational_number = true;
+                good_rational_number = true;//allow to exit the try catch loop
             } catch (Exception e) {
                 System.out.println("please re-enter a rational number");
             }
@@ -65,49 +78,70 @@ public class RationalCalculator {
         return returned_rational;
     }
 
-
+    /**
+     * this function gets the two rational numbers and prints the results of all the operations
+     *
+     * @param first  the first operand in all the operations
+     * @param second the second operand in all the operations
+     */
     private static void showOperations(Rational first, Rational second) {
+
+
+        System.out.println(first.toString() + " > " + second.toString() + " is a " + first.greaterThan(second) + " statement ");
+        System.out.println(first.toString() + " = " + second.toString() + " is a " + first.equals(second) + " statement");
         System.out.println(first.toString() + " + " + second.toString() + "=" + first.plus(second).reduce().toString());
         System.out.println(first.toString() + " - " + second.toString() + "=" + first.minus(second).reduce().toString());
         System.out.println(first.toString() + " * " + second.toString() + "=" + first.multiply(second).reduce().toString());
-        System.out.println(first.toString() + " > " + second.toString() + "=" + first.greaterThan(second));
-        System.out.println(first.toString() + " = " + second.toString() + "=" + first.equals(second));
-
+        try {//division might throw an exception if the second number is a 0/x for some x!=0
+            Rational division_result = first.divide(second);
+            System.out.println(first.toString() + " / " + second.toString() + "=" + division_result.reduce().toString());
+        } catch (ArithmeticException e) {
+            System.out.println("Cant divide by zero");
+        }
     }
 
-
+    /**
+     * This function gets the user's input and handles an 'exit' statement and a blank line input
+     * blank line- ignore
+     * exit-exit the program
+     *
+     * @return a String containing the user's input String
+     */
     public static String GetUserInput() {
-        String user_input = scanner.nextLine().trim();
+        String user_input = scanner.nextLine().trim();// get the line and trim it from white chars
 
-        if (user_input.equals("exit")) {
+        if (user_input.equals("exit")) {//handles exit statement
 
             System.out.println("bye bye");
             System.exit(0);
         }
 
-        if (user_input.equals(""))
+        if (user_input.equals(""))//handle white chars line
             return GetUserInput();
 
         return user_input;
 
     }
 
-
-    private static int getUserInt() {
+    /**
+     * This function uses @GetUserInput and handles converting it to an int and returning it
+     *
+     * @return the int gotten from the user
+     */
+    public static int getUserInt() {
         int returned_value = 0;
         boolean isGoodInput = false;
-        String user_input;
+        String user_input;//determines if to exit the try-catch loop
 
         while (!isGoodInput) {//get input - im not immediately getting an int because I want the user to be able to peacefully exit the program at all times with "exit"
-            try {
+            try {// handle a bad int conversion
                 user_input = GetUserInput();
                 returned_value = Integer.parseInt(user_input);
-                isGoodInput = true;
+                isGoodInput = true;//good number and exit the try-catch loop
             } catch (Exception e) {
                 System.out.println("bad input. please re-enter number.");
             }
         }
-
         return returned_value;
     }
 }
